87a878c6cee286c9bb82bb8ccf43a7ad441b9c89
Spencer <sweinstein34@gmail.com>
2025-12-05 15:24:12 -0500

    exercise 16

--- app.js
+++ app.js
@@ -254,3 +254,13 @@ game.gyms.forEach((gym) => {
     gym.completed = true;
   }
 });
+
+/*
+Exercise 16
+1. Log the entire `game` object to the console. Take a moment to review the changes you've made throughout the exercises.
+
+
+Solve Exercise 16 here:
+*/
+
+console.log(game);
